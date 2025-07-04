be554f54bcbd877a84011445e4633ea9f85ae7e2
Boynich <boynich2025@gmail.com>
2025-07-04 16:47:32 -0500

    Agregado veterinaria.html a public y ruta en Express

--- veterinaria.js
+++ veterinaria.js
@@ -1,18 +1,22 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-// const { MongoClient } = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 
 
-// const uri = 'mongodb://localhost:27017';
-// const client = new MongoClient(uri);
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri);
 let db;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public')); // Esto sirve para cargar los archivos HTML, CSS, imágenes desde "public"
 
+app.get('/veterinaria.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'veterinaria.html'));
+});
 
 
 async function connectDB() {
