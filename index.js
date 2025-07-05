5ffd077d1efea34f4a7bcf680bc50927c753167c
Boynich <boynich2025@gmail.com>
2025-07-05 17:14:03 -0500

    se corrigen las dependecias

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.post('/registro', async (req, res) => {
 });
 
 // Iniciar servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
 });
