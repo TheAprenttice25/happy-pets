68fa0bdf219ecd0a7e7e00dc223afdf9437c894f
Boynich <boynich2025@gmail.com>
2025-07-06 03:20:29 -0500

    correccion uri

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // NECESARIO para leer JSON desde el frontend
 
+console.log("URI de Mongo:", process.env.MONGO_URI);
+
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
