81290b8c20afc12ed4aeb52cc6df4d20fb5d4e0f
Boynich <boynich2025@gmail.com>
2025-07-06 14:35:35 -0500

    modificaciones en rutas

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
 
 //  Ruta principal a login
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+  res.sendFile(path.join(__dirname, 'public', '/login.html'));
 });
 
 
